2a392212919703b8be51ec8036b92b91dc9747dc
MarkHooks <hooksma174@gmail.com>
2019-10-02 08:47:11 -0400

    finished fight song

---
+++ src/FightSong.java
@@ -0,0 +1,27 @@
+public class FightSong {
+    public static void songPart1(){
+        System.out.println("Go, team, go!");
+        System.out.println("You can do it.");
+    }
+    public static void songPart2(){
+        System.out.println("You're the best,");
+        System.out.println("In the West.");
+    }
+    public static void space(){
+        System.out.println("");
+    }
+    public static void main(String[] args) {
+        songPart1();
+        space();
+        songPart1();
+        songPart2();
+        songPart1();
+        space();
+        songPart1();
+        songPart2();
+        songPart1();
+        space();
+        songPart1();
+
+    }
+}
